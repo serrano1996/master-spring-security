d17433d1a6fec2723587cdf4680117f03c196c11
rssoft <rafa.serrano1996@gmail.com>
2024-02-01 19:47:03 +0100

    feat: Added AuthenticationManager and entities user with role and rolepermission

---
+++ src/main/java/com/rssoft/masterspringsecurity/config/security/SecurityBeansInjector.java
@@ -0,0 +1,57 @@
+package com.rssoft.masterspringsecurity.config.security;
+
+import com.rssoft.masterspringsecurity.exceptions.ObjectNotFoundException;
+import com.rssoft.masterspringsecurity.persistence.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityBeansInjector {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+
+        return authenticationConfiguration.getAuthenticationManager();
+
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+
+        DaoAuthenticationProvider authenticationStrategy = new DaoAuthenticationProvider();
+        authenticationStrategy.setPasswordEncoder(passwordEncoder());
+        authenticationStrategy.setUserDetailsService(userDetailsService());
+
+        return authenticationStrategy;
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+
+        return new BCryptPasswordEncoder();
+
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        return (username) -> {
+            return userRepository.findByUsername(username)
+                    .orElseThrow(() -> new ObjectNotFoundException("User not found with username " + username));
+        };
+
+    }
+
+}
---
+++ src/main/java/com/rssoft/masterspringsecurity/persistence/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.rssoft.masterspringsecurity.persistence.repositories;
+
+import com.rssoft.masterspringsecurity.persistence.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+}
